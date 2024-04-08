638efe956d42e89c95cab43b23d29febbc9ff344
Guttojss <guttojsstfg@gmail.com>
2024-04-08 17:15:23 +0100

    Templates com as roots

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request,url_for,render_template,redirect
+from flask import Flask, request,url_for,render_template,redirect,session
 from markupsafe import escape
 # app 
 app = Flask(__name__,static_folder='static',static_url_path='')
-
+'''
 # Definindo a função para fazer o login
 def show_info():  # erro
     return f'Logged as Desconhecido!'
@@ -37,14 +37,14 @@ def hello():
 @app.route('/user/<username>')
 def profile(username):
     return f'{username}\'s profile'
-'''
+
 # Executando o contexto de teste para gerar URLs
 with app.test_request_context():
     print(url_for('index'))
     print(url_for('login'))
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe')) #dá print na consola (tenho q ir ver)
-'''
+
 
 # Rota de exibição de postagem
 @app.route('/post/<int:post_id>')
@@ -63,21 +63,44 @@ def login(info):
         return check_info(info)
 
 with app.test_request_context('/hello', method='POST'):
-    # now you can do something with the request until the
-    # end of the with block, such as basic assertions:
     assert request.path == '/hello'
     assert request.method == 'POST'
 
-'''
+
 @app.route('/upload',methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['note.txt']
         file.save("note.txt")
-'''
 
 @app.route('/')
 def index():
     username = request.cookies.get('username')
     return redirect('static/index.html') #Não Funciona
-'''Aparece uma coisa na Consola qnd se dá F5'''
+Aparece uma coisa na Consola qnd se dá F5'''
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('logged'))
+    return render_template('loggin.html')
+
+@app.route('/logged')
+def logged():
+    if 'username' in session:
+        return render_template('logged.html',name=session['username'])
+    return 'You are not logged in'
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
